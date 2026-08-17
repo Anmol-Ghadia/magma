a921b0a2c703167b0905124643aa27395994f9af
Anmol Ghadia <aghadia@aloe.cs.ubc.ca>
2026-08-17 14:13:05 -0700

    update: libtiff supports separate building of lib and harness

---
+++ targets/libtiff/build_lib.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+
+##
+# Pre-requirements:
+# - env TARGET: path to target work dir
+# - env OUT: path to directory where artifacts are stored
+# - env CC, CXX, FLAGS, LIBS, etc...
+##
+
+if [ ! -d "$TARGET/repo" ]; then
+    echo "fetch.sh must be executed first."
+    exit 1
+fi
+
+WORK="$TARGET/work"
+rm -rf "$WORK"
+mkdir -p "$WORK"
+mkdir -p "$WORK/lib" "$WORK/include"
+
+cd "$TARGET/repo"
+./autogen.sh
+./configure --disable-shared --enable-static --prefix="$WORK"
+make -j$(nproc) clean
+script -q -e -c "make" "$OUT/build_output.log"
+# script instead of direct make because afl does not print the # of instrumented
+# locations unless a stderr is attached
+make install
+
+cp "$WORK/bin/tiffcp" "$OUT/"
